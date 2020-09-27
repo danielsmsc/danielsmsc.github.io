c486a6ec9ea172befbbbd856f4eceea03af29d09
Daniel Skrypnik <71121166+danielsmsc@users.noreply.github.com>
2020-09-27 16:59:45 +0300

    Add files via upload

---
+++ precache-manifest.2c9f7985d63fa125b7a6467e10cd6e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdea8924ce0b1a30cbedb2aca61726c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d40d329a51a77c00e0b1",
+    "url": "/static/css/main.59d7deed.chunk.css"
+  },
+  {
+    "revision": "bb26e6155e499841d4ee",
+    "url": "/static/js/2.13404ac6.chunk.js"
+  },
+  {
+    "revision": "41a1cf14bda7df3f2390d7c0badf734d",
+    "url": "/static/js/2.13404ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40d329a51a77c00e0b1",
+    "url": "/static/js/main.1ae03189.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
